f5b0ba2092db360846ed88e4d354b88a7e7b9dc2
Bharath Saiguhan <bharathsaiguhan@gmail.com>
2021-09-01 21:46:26 +0530

    (prog) changes for S-1.4

--- src/fahr2cels-for.c
+++ src/fahr2cels-for.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+// Use symbolic names to define 'magic' numbers
+#define  LOWER  0       /* lower limit of table  */
+#define  UPPER  300     /* upper limit */
+#define  STEP   20      /* step size */
+
 // print Fahrenheit-Celsius table
 
 int main()
@@ -7,9 +12,9 @@ int main()
         int fahr;
 
         // The 'for' construction
-        /*   init.     check        update          */
-        for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        /*   init.         check          update            */
+        for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
                 // Note: every expression returns a value, which is why
-                //       the third argument is valid (it returns a float)
+                //       the third argument is valid (returns a float)
                 printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }

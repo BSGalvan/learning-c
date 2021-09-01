297630a3f4f5a5a7c7e0fc98072332b7e9aecdb5
Bharath Saiguhan <bharathsaiguhan@gmail.com>
2021-09-01 13:28:04 +0530

    add 'for' version

---
+++ src/fahr2cels-for.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// print Fahrenheit-Celsius table
+
+int main()
+{
+    int fahr;
+
+    // The 'for' construction
+    /*   init.     check        update          */
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        // Note: every expression returns a value, which is why
+        //       the third argument is valid (it returns a float)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+}

ac22db3a0bd735f851665f81b42b10089e8ad605
Bharath Saiguhan <bharathsaiguhan@gmail.com>
2021-09-01 21:31:16 +0530

    (prac) complete exercise 1-5

--- src/fahr2cels.c
+++ src/fahr2cels.c
@@ -5,27 +5,27 @@
 
 int main()
 {
-	// int -> 16/32-bit, lies in [-32768, 32767] (-2^16 to 2^16-1)
-	float fahr, celsius;
-	int lower, upper, step;
+        // int -> 16/32-bit, lies in [-32768, 32767] (-2^16 to 2^16-1)
+        float fahr, celsius;
+        int lower, upper, step;
 
-	lower = 0;		/* lower limit of temperature table */
-	upper = 300;		/* upper limit */
-	step = 20;		/* step size  */
+        lower = 0;      /* lower limit of temperature table */
+        upper = 300;    /* upper limit */
+        step = 20;      /* step size  */
 
-	fahr = lower;
+        fahr = lower;
 
-	printf("#####################################################\n");
-	printf("#         Fahrenheit --> Celsius Conversion         #\n");
-	printf("#####################################################\n");
+        printf("#####################################################\n");
+        printf("#         Fahrenheit --> Celsius Conversion         #\n");
+        printf("#####################################################\n");
 
-	while (fahr <= upper) {
-		// (fahr - 32) would get converted implicitly just as well
-		celsius = (5.0 / 9.0) * (fahr - 32.0);
-		// see printf(1) for more formatting details
-		// Tip: basically %x.yf means print a float with a field
-		//      x chars wide (and) y chars after decimal point
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
-	}
+        while (fahr <= upper) {
+                // (fahr - 32) would get converted implicitly just as well
+                celsius = (5.0 / 9.0) * (fahr - 32.0);
+                // see printf(1) for more formatting details
+                // Tip: basically %x.yf means print a float with a field
+                //      x chars wide (and) y chars after decimal point
+                printf("%3.0f %6.1f\n", fahr, celsius);
+                fahr = fahr + step;
+        }
 }

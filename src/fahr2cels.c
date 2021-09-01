2e6f027ba6a3664d5838a634b65ca76ba4b2b5b4
Bharath Saiguhan <bharathsaiguhan@gmail.com>
2021-09-01 12:45:22 +0530

    (minor) comments + formatting

--- src/fahr2cels.c
+++ src/fahr2cels.c
@@ -5,17 +5,18 @@
 
 int main()
 {
+    // int -> 16/32-bit, lies in [-32768, 32767] (-2^16 to 2^16-1)
     int fahr, celsius;
     int lower, upper, step;
 
     lower = 0;   /* lower limit of temperature table */
     upper = 300; /* upper limit */
-    step = 20;   /*  step size  */
+    step = 20;   /* step size  */
 
     fahr = lower;
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%d\t%d\n", fahr, celsius); // see printf(1) for more formatting details
         fahr = fahr + step;
     }
 }
--- src/hello.c
+++ src/hello.c
@@ -3,5 +3,4 @@
 int main()  // main() returns an int type implicitly
 {
     printf("Hello, world!\n");
-    int count = 9;
 }

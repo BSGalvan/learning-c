34c84474fb3cd38e9e295c64011c380fe6096149
Bharath Saiguhan <bharathsaiguhan@gmail.com>
2021-09-03 18:15:31 +0530

    (prog) changes for S-1.5.2 v2

--- src/char_count.c
+++ src/char_count.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2nd version */
 
 int main()
 {
         /* Atleast 32-bits, so enough for a counter */
         /* Could use a double for really large inputs */
-        long nc;
+        double nc;
 
-        nc = 0;
-        while (getchar() != EOF)
-                /* prefix increment operator */
-                ++nc;
-        /* %ld for a long int */
-        printf("%ld\n", nc);
+        for (nc = 0; getchar() != EOF ; ++nc)
+                /* Do nothing; need this since for-loop */
+                /* cannot be empty */
+                ;
+        /* Print no decimal places */
+        printf("%.0f\n", nc);
+
+        /* Note: for zero-length input, the program outputs 0, */
+        /*       as it should! */
 }

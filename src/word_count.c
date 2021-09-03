850d18f80f28b8301e3c6f2244d2d362efba5265
Bharath Saiguhan <bharathsaiguhan@gmail.com>
2021-09-04 00:40:22 +0530

    (prog) changes for S-1.5.4

---
+++ src/word_count.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define IN  1  /* inside a word */
+#define OUT 0  /* outside a word */
+
+/* count lines, words and characters in input */
+/* Note: a word here is any sequence of characters */
+/*       without a blank, newline or tab in it which means that punctuation */
+/*       will screw up the count (Exercise 1-11!)*/
+
+int main()
+{
+        int c, nl, nw, nc, state;
+
+        state = OUT;
+        /* chained assignments possible, since each one is an expression */
+        nl = nw = nc = 0;
+        while ((c = getchar()) != EOF) {
+                ++nc;
+                if (c == '\n')
+                        ++nl;
+                if (c == ' ' || c == '\n' || c == '\t')
+                        state = OUT;
+                else if (state == OUT) {
+                        state = IN;
+                        ++nw;
+                }
+        }
+        printf("Number of lines: %d\n", nl);
+        printf("Number of words: %d\n", nw);
+        printf("Number of characters: %d\n", nc);
+}
